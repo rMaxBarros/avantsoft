bcbf095b34b84bc65eb60c172aa4badea1e499a7
rMaxBarros <rmaxbarros@gmail.com>
2025-07-31 17:08:35 -0300

    fix: Modal adjustments

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,8 +2,8 @@ import styles from './Modal.module.css';
 
 function Modal({ message, onClose}) {
     return (
-        <div className="styles.overlay">
-            <div className="styles.modal">
+        <div className={styles.overlay}>
+            <div className={styles.modal}>
                 <p>{message}</p>
                 <button onClick={onClose}>Close</button>
             </div>
