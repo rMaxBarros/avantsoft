4b23bb81ed71f920873852aa21b7b4faef07245f
rMaxBarros <rmaxbarros@gmail.com>
2025-07-31 10:43:32 -0300

    feat: setting up the application module and global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
 }
 bootstrap();
+
+// The main entry point for the NestJS application, setting up the application module and global validation pipe.
+// It listens on port 3000 for incoming requests and initializes the application with necessary configurations.
